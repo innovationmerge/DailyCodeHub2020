ba6680f19123ec10d08280cee0afad7d56047a8d
innovationmerge <innovationmerge@gmail.com>
2021-11-28 13:11:04 +0530

    301

--- 303_DAY_303_Linux_touch_access_time/linux_touch.sh
+++
@@ -1,8 +0,0 @@
-# **************** "iNNovationMerge DailyCodeHub" ****************
-# Visit https://www.innovationmerge.com/
-# Theme : File operations using Linux Touch
-# Change access time of a file using touch in Ubuntu
-
-$ touch -a test-1.txt
-
-
--- 305_DAY_305_Linux_touch_mod_time/linux_touch.sh
+++
@@ -1,8 +0,0 @@
-# **************** "iNNovationMerge DailyCodeHub" ****************
-# Visit https://www.innovationmerge.com/
-# Theme : File operations using Linux Touch
-# Change modification time of a file using touch in Ubuntu
-
-$ touch -m test-1.txt
-
-
