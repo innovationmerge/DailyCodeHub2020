b73ea36eba803f166ee06120ae52f1f78c8e64ef
innovationmerge <innovationmerge@gmail.com>
2021-11-28 13:09:09 +0530

    301

---
+++ 301_DAY_301_Linux_touch1/linux_touch.sh
@@ -0,0 +1,8 @@
+# **************** "iNNovationMerge DailyCodeHub" ****************
+# Visit https://www.innovationmerge.com/
+# Theme : File operations using Linux Touch
+# Create an empty file using touch in Ubuntu
+
+$ touch test.txt
+
+
